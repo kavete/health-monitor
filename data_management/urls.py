59e5c8a2129e12a44b369b24e82ccece50b0315a
kavete <kavetebrian@gmail.com>
2025-05-31 14:31:38 +0300

    Added ward details page

--- data_management/urls.py
+++ data_management/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from data_management import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
+    path('ward', views.ward_details, name='ward_detail'),
 ]
+
+# /<slug:ward_slug>/
--- data_management/views.py
+++ data_management/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def dashboard(request):
     context = {}
     return render(request, 'data_management/index.html', context)
+
+
+def ward_details(request):
+    context = {}
+    return render(request, 'data_management/ward-details.html', context)
