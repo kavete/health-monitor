f7bc64cb8f8b8b6c4361672338fb75b5c4c6696c
kavete <kavetebrian@gmail.com>
2025-06-01 19:56:57 +0300

    Added patient vitals in ward detail page

--- data_management/urls.py
+++ data_management/urls.py
@@ -3,7 +3,7 @@ from data_management import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path('ward', views.ward_details, name='ward_detail'),
+    path('ward/<slug:ward_slug>/', views.ward_details, name='ward_detail'),
 ]
 
-# /<slug:ward_slug>/
+# 
--- data_management/views.py
+++ data_management/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Ward, Patient, Doctor, Microcontroller, Bed, WardReading, PatientVitals
+from django.db.models import Max
 
 def dashboard(request):
-    context = {}
+    ward_count = Ward.objects.count()
+    patient_count = Patient.objects.count()
+    doctor_count = Doctor.objects.count()
+    bed_count = Bed.objects.count()
+    microcontroller_count = Microcontroller.objects.count()
+
+    latest_per_ward = (
+        WardReading.objects.values('ward')
+        .annotate(latest=Max('timestamp'))
+    )
+    # Build a list of latest WardCondition objects for each ward
+    latest_conditions = [
+        WardReading.objects.filter(ward=entry['ward'], timestamp=entry['latest']).first()
+        for entry in latest_per_ward
+    ]
+    context = {
+        'ward_conditions': latest_conditions,
+        'ward_count': ward_count,
+        'patient_count': patient_count,
+        'doctor_count': doctor_count,
+        'bed_count': bed_count,
+        'microcontroller_count': microcontroller_count,
+    }
     return render(request, 'data_management/index.html', context)
 
 
-def ward_details(request):
-    context = {}
+def ward_details(request, ward_slug):
+    ward = get_object_or_404(Ward, slug=ward_slug)
+    latest_condition = WardReading.objects.filter(ward=ward).order_by('-timestamp').first()
+    patients_in_ward = Patient.objects.filter(bed__ward=ward).select_related('bed')
+
+    patient_vitals = []
+    for patient in patients_in_ward:
+        latest_vital = PatientVitals.objects.filter(patient=patient).order_by('-timestamp').first()
+        if latest_vital:
+            patient_vitals.append(latest_vital)
+    context = {
+        'ward_name': ward.name,
+        'ward_slug': ward.slug,
+        'latest_condition': latest_condition,
+        "patient_vitals": patient_vitals,
+    }
+    
     return render(request, 'data_management/ward-details.html', context)
